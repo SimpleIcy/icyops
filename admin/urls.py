6d8f9ce0fdcc456f4ee1379a44ab4f032e07755d
icy <d360478265@153.com>
2018-05-21 17:26:51 +0800

    start some app

--- admin/urls.py
+++ admin/urls.py
@@ -1,4 +1,4 @@
-"""icyops URL Configuration
+"""admin URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
